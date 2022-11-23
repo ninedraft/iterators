336758dd29de552c29db2d7116a9f1b80d8fa01e
merlin <clamores.pro@gmail.com>
2022-11-23 18:49:40 +0300

    add zip

--- zip.go
+++ zip.go
@@ -4,7 +4,7 @@ import "context"
 
 // Zip combines two sequences into sequence of pairs.
 // If any of iterators is drained, then resulting iterator will return false.
-func Zip[A, B any](aa Iterator[A], bb Iterator[B]) Iterator[Pair[A, B]] {
+func Zip[A, B any](aa Iterator[A], bb Iterator[B]) Pairs[A, B] {
 	return Fn[Pair[A, B]](func(ctx context.Context) (Pair[A, B], bool) {
 		var a, aOk = aa.Next(ctx)
 		if !aOk {
